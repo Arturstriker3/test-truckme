ca368f996b928323ac6b643c9e72800d64ea9b80
Arturstriker3 <artur.danielgama@gmail.com>
2024-07-12 10:28:34 -0300

    feat: - Project structure and layers - Color palette - Global css config

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,41 @@
 </script>
 
 <template>
-  <h1>Truck</h1>
+  
+  <header>
+    <div class="container" >
+      <nav>
+        <div class="logo">
+          <img src="./assets/images/logo_truck_me_frete_plataforma.png" alt="truckme-logo">
+          <h2><span>truck</span>me</h2>
+        </div>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Serviços</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
 </template>
 
-<style scoped>
+<style lang="scss" >
+
+@import "./style.scss";
+
+header {
+    background-color: $white-color;
+}
+
+.button {
+    background-color: $primary-color;
+    color: $black-color;
+
+    &:hover {
+        background-color: $gray-dark-color;
+    }
+}
 
 </style>
