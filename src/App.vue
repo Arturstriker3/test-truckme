36f067359925dd0d96b4ef9764b84c238d43fa95
Arturstriker3 <artur.danielgama@gmail.com>
2024-07-19 08:43:30 -0300

    fix: small fixes

--- src/App.vue
+++ src/App.vue
@@ -143,7 +143,9 @@ watch(isMenuOpen, (newVal) => {
             <h3>Serviços</h3>
           </div>
           <div class="content-body">
-            <div class="grid-item">Item 1</div>
+            <div class="grid-item">
+              titulo
+            </div>
             <div class="grid-item">Item 2</div>
             <div class="grid-item">Item 3</div>
             <div class="grid-item">Item 4</div>
